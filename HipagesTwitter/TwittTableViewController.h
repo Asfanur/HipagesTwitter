10314fa808aa1529b929451ebb26cbe5720eab23
Asfanur <asfanur.arafin@gmail.com>
2015-06-13 17:29:55 +1000

    initial commit

---
+++ HipagesTwitter/TwittTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwittTableViewController.h
+//  HipagesTwitter
+//
+//  Created by Asfanur Arafin on 13/06/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwittTableViewController : UITableViewController
+
+@end
